0ac819e9f9fb315f9e40909468c5c380d90baefc
ANIL-RONGALA <anilr.edu2023@gmail.com>
2025-09-07 21:20:17 -0500

    inputs as int 8 and output as int32

--- Golden/golden_matmul.py
+++ Golden/golden_matmul.py
@@ -3,5 +3,8 @@ import numpy as np
 def matrix_multiply(A, B):
     """ Matrix multiplication operation defined """
 
-    C = np.matmul(A, B);
+    A = A.astype(np.int8) 
+    B = B.astype(np.int8)
+
+    C = np.matmul(A, B, dtype=np.int32)
     return C

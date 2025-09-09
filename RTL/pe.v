d12e32ddcac98fcdd531681c06446de82b8ca1c1
ANIL-RONGALA <anilr.edu2023@gmail.com>
2025-09-08 23:46:53 -0500

    pe file format

---
+++ RTL/pe.v
@@ -0,0 +1,14 @@
+module pe(input clk,
+            input rst,
+            input[7:0] a_input,
+            input[7:0] b_input,
+            output[31:0] c_out
+            );
+
+
+for(i=0;i<=n;i++)
+for(j=0;j<=m,j++)
+
+end
+ens
+endmodule

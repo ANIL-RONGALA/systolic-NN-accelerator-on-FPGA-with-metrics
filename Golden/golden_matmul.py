6fb6539e709eae8a5ae00469d4491398300234cf
ANIL-RONGALA <anilr.edu2023@gmail.com>
2025-09-13 23:45:23 -0500

    update matmul function

--- Golden/golden_matmul.py
+++ Golden/golden_matmul.py
@@ -1,10 +1,20 @@
 import numpy as np
 
-def matrix_multiply(A, B):
-    """ Matrix multiplication operation defined """
-
-    A = A.astype(np.int8) 
+def matrix_multiply(A: np.ndarray, B: np.ndarray) -> np.ndarray:
+    """
+    Performs standard matrix multiplication using NumPy.
+    This is the golden reference model.
+    """
+    # Force data type to int8 for inputs
+    A = A.astype(np.int8)
     B = B.astype(np.int8)
 
+    # Accumulate in int32 to mimic hardware accumulation
     C = np.matmul(A, B, dtype=np.int32)
     return C
+
+if __name__ == "__main__":
+    # Quick sanity test
+    A = np.array([[1, 2], [3, 4]], dtype=np.int8)
+    B = np.array([[5, 6], [7, 8]], dtype=np.int8)
+    print(matrix_multiply(A, B))
